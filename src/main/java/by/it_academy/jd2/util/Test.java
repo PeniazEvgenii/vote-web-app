ff3c0e9f3f574c6a4f00b94421fc09a13029a059
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-08-06 14:21:39 +0300

    OLD storages remove to folder old and add annotation @Depricated for them

--- src/main/java/by/it_academy/jd2/util/Test.java
+++ src/main/java/by/it_academy/jd2/util/Test.java
@@ -1,12 +1,18 @@
 package by.it_academy.jd2.util;
 
 import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
 
         try (InputStream inputStream = Test.class.getClassLoader().getResourceAsStream("ArtistsForTestLoad");
              BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            List<String> collect = bufferedReader.lines()
+                    .filter(str -> !str.isBlank())
+                    .collect(Collectors.toList());
+            System.out.println(collect);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
